5a6a5e95984350a0d92bc78850436f5babdbd9dd
Axel Etcheverry <euskadi31@gmail.com>
2014-08-30 23:12:27 +0200

    Initial Commit

---
+++ Redis/Redis.h
@@ -0,0 +1,13 @@
+//
+//  Redis.h
+//  Redis
+//
+//  Created by Axel Etcheverry on 30/08/2014.
+//  Copyright (c) 2014 Axel Etcheverry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Redis : NSObject
+
+@end
---
+++ Redis/Redis.m
@@ -0,0 +1,13 @@
+//
+//  Redis.m
+//  Redis
+//
+//  Created by Axel Etcheverry on 30/08/2014.
+//  Copyright (c) 2014 Axel Etcheverry. All rights reserved.
+//
+
+#import "Redis.h"
+
+@implementation Redis
+
+@end
